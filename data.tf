3d68a3b1e80c32912290b4fd1d419312675d2bb8
shaazAhmed91 <shaaz.ahmed573@gmail.com>
2025-10-31 19:26:50 +0530

    data ami-name

--- data.tf
+++ data.tf
@@ -1,3 +1,9 @@
 data "aws_ami" "ubuntu"{
-    
+    most_recent = true
+    owners = ["amazon"]
+
+}
+filter={
+    name = "name"
+    image = [ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*]
 }
